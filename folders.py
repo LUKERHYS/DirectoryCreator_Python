ee4596269967cc2eb16e2f36e72e192bf4c7e0e1
LUKERHYS <rhystechnology@gmail.com>
2018-02-17 15:16:58 +0000

    The code now creates separate directories based on human input delimited by spaces and prefixes them with Batch_. At this point it only creates the directories in a predefined / hard coded location

--- folders.py
+++ folders.py
@@ -1,50 +1,18 @@
+# Imports Here
 import os
-os.chdir('C:\Users\GC4C-0013\Desktop\New folder')
+
+# Folder Creation Destination
+os.chdir('/Users/lukerhys/Desktop/test')
+
+# Custom Objects
 batches = ('')
 prefix = "Batch_"
+
+# For loop to prompt human input of directory names
 for i in range(1):
-	batches = raw_input("How many batches are you importing?: ")
-	#input_list = batches.split()
-	#input_list = [int(a) for a in input_list]
-	batchNames = prefix + batches #input_list
-#print batchNames
-os.mkdir(batchNames)
+	batches_num = input("Which batches are you importing?:  ")
 
-
-
-
-
-
-
-
-"""
-#batchType = raw_input("Are you importing Irons, Bags or a Batch?: ")
-print "How many batches are you importing?"
-batchCount = raw_input() #splits the input string on spaces
-input_list = batchCount.split()
-# process string elements in the list and make them integers
-input_list = [int(a) for a in input_list]
-# for each item in the list of items
-for value in batchCount:
-
-
-batchCount = []
-for i in range(1):
-	numOfBatches = raw_input("How many batches are you importing?: ")
-	batchCount.append(numOfBatches)
-break
-	return batchCount
-
-	 construct the filename; prefix or suffix optional
-filename = 'batch_' + batchCount
-	 open the file to be written
-directory = open(filename, 'w')
-	write the content in the file including the value being passed to each; %s indicates a string
-directory.write('%s' % batchCount)
-
-
-	# example using an iframe
-	# fo.write('<iframe src="http://www.washingtonpost.com/wp-srv/special/politics/election-map-2012/homepage/embed-map.html?mapstate=%s" width="610" height="502" scrolling="no" frameborder="0"></iframe>' % state)
-
-	close the file
-"""
+# For loop to ad the batch prefix and create the directories
+for f in batches_num.split():
+	fileName = prefix + f
+	os.mkdir(fileName)
