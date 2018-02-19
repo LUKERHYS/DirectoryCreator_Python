c4dee738fcb2a453bd35cdbef5bac5bfb4c2af19
LUKERHYS <rhystechnology@gmail.com>
2018-02-19 14:48:25 +0000

    I have now removed the hard-coded path in the error checking in V7 which I have now updated this is v8

--- folders_v8.py
+++ folders_v8.py
@@ -1,15 +1,10 @@
 # Imports Here
 import os
-# Directory Creation Location
-os.chdir(input('Where would you like to create the directories?: '))
-
 # Directory Prefix Variable can be anything you want
 prefix = input("How would you like to prefix your directories?: ")
-
 # Input string as interger  - converted to range
 batch_num = int(input("How many batches are you importing?: "))
 output = batch_num + 1
-
 # For loop to add the batch prefix and create the directories
 for f in range(1, output, 1):
 	fileName = prefix + "_" + str(f)
@@ -17,3 +12,11 @@ for f in range(1, output, 1):
 		print("Err: File already exists. Please try again.")
 	else:
 		os.mkdir(fileName)
+if not os.path.exists(location):
+	answer = print("This directory does not exist. Would you ilke to create it? Answer y or n:  ")
+	if answer == "y":
+		os.makedirs(directory)
+	else:
+		print("Error, Failed to create directories.")
+else:
+	end
