a0ad923d815f01d86884a2fa9b2d114e0e2552c8
LUKERHYS <rhystechnology@gmail.com>
2018-05-02 13:49:46 +0100

    version 8 now gives the option to start at a given position, for example if you import 3 batches earlier in the day an then require a further 3 to be made with a unique int

--- folders_v8.py
+++ folders_v8.py
@@ -1,14 +1,24 @@
+# Imports Here
 import os
 
-file_path = os.chdir(input('Where would you like to create the directories?: '))
-cwd = os.getcwd(os.chdir(input('Where would you like to create the directories?: ')))
-print(file_path)
+# Directory Creation Location
+os.chdir(input('Where would you like to create the directories?: '))
 
-"""
-directory = os.path.dirname(file_path)
+# Directory Prefix Variable can be anything you want
+prefix = input("How would you like to prefix your directories?: ")
 
-def ensure_dir(file_path):
-    directory = os.path.dirname(file_path)
-	if not os.path.exists(directory):
-    os.makedirs(directory)
-"""
+# Input string as interger  - converted to range
+batch_num = int(input("How many batches are you importing?: "))
+output = batch_num + 1
+
+# Where would you like to start the batch count
+start_point = int(input("How many batches do you have so far?: "))
+start_num =start_point + output
+
+# For loop to add the batch prefix and create the directories
+for f in range(1, start_num, 1):
+	fileName = prefix + "_" + str(f)
+	if os.path.exists(fileName):
+		print("Err: File already exists. Please try again.")
+	else:
+		os.mkdir(fileName)
