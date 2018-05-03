efc2a8d4d2603633982357e098e854457836a8ff
Luke Rhys Melvin <rhystechnology@gmail.com>
2018-05-03 18:00:59 +0100

    Work in progress!
    Work in progress!

--- folders_v8.py
+++ folders_v8.py
@@ -2,10 +2,10 @@
 import os
 
 # Directory Creation Location
-os.chdir(input('Where would you like to create the directories?: '))
+os.chdir(raw_input("Where would you like to create the directories?: "))
 
 # Directory Prefix Variable can be anything you want
-prefix = input("How would you like to prefix your directories?: ")
+prefix = raw_input("How would you like to prefix your directories?: ")
 
 # Input string as interger  - converted to range
 batch_num = int(input("How many batches are you importing?: "))
@@ -13,10 +13,13 @@ output = batch_num + 1
 
 # Where would you like to start the batch count
 start_point = int(input("How many batches do you have so far?: "))
-start_num =start_point + output
+	if start_point == 0:
+		correct_start = start_point + 1
+	else:
+		None
 
 # For loop to add the batch prefix and create the directories
-for f in range(1, start_num, 1):
+for f in range(start_point, output, 1):
 	fileName = prefix + "_" + str(f)
 	if os.path.exists(fileName):
 		print("Err: File already exists. Please try again.")
