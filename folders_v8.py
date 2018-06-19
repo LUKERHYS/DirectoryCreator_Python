1f128cd722bcbc61039b11e3305ba6ab013a1c2a
Luke Rhys Melvin <rhystechnology@gmail.com>
2018-06-19 17:12:55 +0100

    V8 Fix
    v8 Will now allow you to create one batch incrementally but split across several imports.
    I.e. create directory 1-3 then come back and tell the programme you want to create 4-6 because you already have 1,2 and 3.

--- folders_v8.py
+++ folders_v8.py
@@ -7,19 +7,16 @@ os.chdir(raw_input("Where would you like to create the directories?: "))
 # Directory Prefix Variable can be anything you want
 prefix = raw_input("How would you like to prefix your directories?: ")
 
-# Input string as interger  - converted to range
-batch_num = int(input("How many batches are you importing?: "))
-output = batch_num + 1
-
 # Where would you like to start the batch count
 start_point = int(input("How many batches do you have so far?: "))
-	if start_point == 0:
-		correct_start = start_point + 1
-	else:
-		None
+start_num = start_point + 1
+
+# Input string as interger  - converted to range
+batch_num = int(input("How many batches are you importing?: "))
+output = batch_num + start_point + 1
 
 # For loop to add the batch prefix and create the directories
-for f in range(start_point, output, 1):
+for f in range(start_num, output, 1):
 	fileName = prefix + "_" + str(f)
 	if os.path.exists(fileName):
 		print("Err: File already exists. Please try again.")
